982d3ed416338b045150de6bff3d1c050401ba3a
NT14 <n_tahar@outlook.com>
2022-02-11 21:49:44 +0000

    event listener for search button

--- Script.js
+++ Script.js
@@ -3,3 +3,6 @@ const button = document.getElementById("submit");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "vALoXTDCwMtpJAXLFXIA2X2UcOi3G0Pb";
 const openWeatherAPIKey = "86cf18e05a581bc89bba25b5d33cbc10";
+
+// Search button event listener to run function search handler
+button.addEventListener("click", searchHandler);
