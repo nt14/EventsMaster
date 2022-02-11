2d81aff396acd313e319d44aa99957f2c0dddaa9
NT14 <n_tahar@outlook.com>
2022-02-11 22:16:57 +0000

    targeting video on html

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
 // // Targeting html elements by id and class
 const locationInputEl = document.getElementById("locationInput");
 const button = document.getElementById("submit");
+let video = document.querySelector("video");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "vALoXTDCwMtpJAXLFXIA2X2UcOi3G0Pb";
 const openWeatherAPIKey = "86cf18e05a581bc89bba25b5d33cbc10";
