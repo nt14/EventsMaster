f56bb5d3c54e0536f4feec8162b1c3739498d2b3
NT14 <n_tahar@outlook.com>
2022-02-11 22:14:11 +0000

    Function to be excuted once search btn is clicked

--- Script.js
+++ Script.js
@@ -4,6 +4,8 @@ const button = document.getElementById("submit");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "vALoXTDCwMtpJAXLFXIA2X2UcOi3G0Pb";
 const openWeatherAPIKey = "86cf18e05a581bc89bba25b5d33cbc10";
+// Function to get events from ticket masters API, and then dynamically displaying data using cards. Function serves as a search handler.
+function searchHandler(e) {
 
 // Search button event listener to run function search handler
 button.addEventListener("click", searchHandler);
