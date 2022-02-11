697cebbdae726ee18de02d6f4db0da20a56e70c3
NT14 <n_tahar@outlook.com>
2022-02-11 21:46:00 +0000

    Targeting html elements by id for button/user input

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
+const locationInputEl = document.getElementById("locationInput");
+const button = document.getElementById("submit");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "vALoXTDCwMtpJAXLFXIA2X2UcOi3G0Pb";
 const openWeatherAPIKey = "86cf18e05a581bc89bba25b5d33cbc10";
