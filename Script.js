fd131d5e6f0db469bba6102c42730c6a84e2f1b8
NT14 <n_tahar@outlook.com>
2022-02-11 22:14:59 +0000

    API fetch for ticket master

--- Script.js
+++ Script.js
@@ -6,6 +6,20 @@ const ticketMasterAPIKey = "vALoXTDCwMtpJAXLFXIA2X2UcOi3G0Pb";
 const openWeatherAPIKey = "86cf18e05a581bc89bba25b5d33cbc10";
 // Function to get events from ticket masters API, and then dynamically displaying data using cards. Function serves as a search handler.
 function searchHandler(e) {
+  fetch(
+    "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" +
+      ticketMasterAPIKey +
+      "&sort=date,asc" +
+      "&city=" +
+      locationInputEl.value +
+      "&countryCode=GB" +
+      "&startedatetime=" +
+      Today +
+      "&size=2"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
 
 // Search button event listener to run function search handler
 button.addEventListener("click", searchHandler);
